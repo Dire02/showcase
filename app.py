1afe3b0ea88ebb9d8406752bff5c3cba3e708aca
Dire <parsas.s.h.g@gmail.com>
2024-10-10 18:05:17 +0330

    changes

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 
 if __name__ == "__main__":
     app.run()
